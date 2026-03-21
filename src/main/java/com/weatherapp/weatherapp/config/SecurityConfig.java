6639b162a2a75cde070e845134835c2a79fac8de
akilaManu <akilamanujith@gmail.com>
2026-03-21 18:26:15 +0530

    fix: Update CORS configuration to allow requests from the new frontend URL

--- src/main/java/com/weatherapp/weatherapp/config/SecurityConfig.java
+++ src/main/java/com/weatherapp/weatherapp/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://ctse-frontend-pos.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
